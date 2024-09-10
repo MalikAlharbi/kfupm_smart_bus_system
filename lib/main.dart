c7ee965c3222edcb22089d904332ac07c3114ca9
Ghali Alharbi <gash20190@gmail.com>
2024-09-10 23:28:44 +0300

    New Edit

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page 412!'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page 412!s'),
     );
   }
 }
