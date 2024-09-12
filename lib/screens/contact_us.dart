097c35c042fd90a331ccc0273c50a19e4e81842c
MalikAlharbi <malik.f.alharbi@gmail.com>
2024-09-12 17:40:21 +0300

    fix: fixed scroll

---
+++ lib/screens/contact_us.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class ContactUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contact Us'),
+      ),
+      body: Center(
+        child: Text('Contact Us'),
+      ),
+    );
+  }
+}
--- lib/screens/widget_main.dart
+++ lib/screens/widget_main.dart
@@ -9,53 +9,6 @@ class Widgetmain extends StatelessWidget {
     return _buildMainContent();
   }
 
-  Widget _buildTopBar() {
-    return Container(
-      margin: const EdgeInsets.all(5.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: const Color(0xFF179C3D),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            blurRadius: 8,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: Row(
-          children: [
-            _buildIconButton(Icons.account_circle_outlined, () {
-              print("Profile tapped");
-            }),
-            const Spacer(),
-            const Image(
-              image: AssetImage("assets/images/kfupm_logo.png"),
-              width: 57,
-            ),
-            const Spacer(),
-            _buildIconButton(Icons.call, () {
-              print("Call tapped");
-            }),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildIconButton(IconData icon, VoidCallback onTap) {
-    return InkWell(
-      onTap: onTap,
-      child: Icon(
-        icon,
-        size: 32,
-        color: Colors.white,
-      ),
-    );
-  }
-
   Widget _buildMainContent() {
     return Container(
       margin: const EdgeInsets.all(16.0),
@@ -74,21 +27,26 @@ class Widgetmain extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Welcoming(),
-            const SizedBox(height: 24),
-            _buildGridSection(
-              [
-                _buildGridItem("Track Buses", Icons.directions_bus),
-                _buildGridItem("Available Events", Icons.event),
-              ],
+             const SizedBox(height: 24),
+            SizedBox(
+              child: _buildGridSection(
+                [
+                  _buildGridItem("Track Buses", Icons.directions_bus),
+                  _buildGridItem("Available Events", Icons.event),
+                ],
+              ),
             ),
             const SizedBox(height: 24),
-            _buildGridSection(
-              [
-                _buildGridItem("Feedback", Icons.feedback),
-                _buildGridItem("Report Problem", Icons.report_problem),
-              ],
+            SizedBox(
+              child: _buildGridSection(
+                [
+                  _buildGridItem("Feedback", Icons.feedback),
+                  _buildGridItem("Report Problem", Icons.report_problem),
+                ],
+              ),
             ),
             const SizedBox(height: 24),
           ],
@@ -99,6 +57,7 @@ class Widgetmain extends StatelessWidget {
 
   Widget _buildGridSection(List<Widget> children) {
     return GridView.count(
+      physics: NeverScrollableScrollPhysics(),
       crossAxisCount: 2,
       crossAxisSpacing: 16,
       mainAxisSpacing: 16,
@@ -108,64 +67,34 @@ class Widgetmain extends StatelessWidget {
   }
 
   Widget _buildGridItem(String title, IconData icon) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(40),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            blurRadius: 4,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(icon, size: 48, color: const Color(0xFF179C3D)),
-          const SizedBox(height: 8),
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
+    return InkWell(
+      onTap: () {
+        print("wORKING !!!");
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(40),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              blurRadius: 4,
+              offset: const Offset(0, 2),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildBottomBar() {
-    return Container(
-      margin: const EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: const Color(0xFF179C3D),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            blurRadius: 8,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: Row(
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _buildIconButton(Icons.directions_bus, () {
-              print("Profile tapped");
-            }),
-            const Spacer(),
-            _buildIconButton(Icons.home, () {
-              print("home");
-            }),
-            const Spacer(),
-            _buildIconButton(Icons.logout, () {
-              print("Call tapped");
-            }),
+            Icon(icon, size: 48, color: const Color(0xFF179C3D)),
+            const SizedBox(height: 8),
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ],
         ),
       ),
