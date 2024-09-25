39f4400fe38bc19ebb19754078af9f17961de258
MalikAlharbi <malik.f.alharbi@gmail.com>
2024-09-25 22:40:24 +0300

    refactor: remove top & bottom bars from WidgetMain

--- lib/Screens/WidgetMain.dart
+++ lib/Screens/WidgetMain.dart
@@ -15,47 +15,9 @@ class Widgetmain extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              _buildTopBar(),
               _buildMainContent(),
-              _buildBottomBar(),
             ],
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildTopBar() {
-    return Container(
-      margin: const EdgeInsets.all(5.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: const Color(0xFF179C3D),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            blurRadius: 8,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: Row(
-          children: [
-            _buildIconButton(Icons.account_circle_outlined, () {
-              print("Profile tapped");
-            }),
-            const Spacer(),
-            Image.network(
-              'https://upload.wikimedia.org/wikipedia/ar/archive/3/37/20180719130502%21King_Fahd_University_of_Petroleum_%26_Minerals_Logo.png',
-              width: 57,
-            ),
-            const Spacer(),
-            _buildIconButton(Icons.call, () {
-              print("Call tapped");
-            }),
-          ],
         ),
       ),
     );
@@ -152,41 +114,6 @@ class Widgetmain extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _buildBottomBar() {
-    return Container(
-      margin: const EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: const Color(0xFF179C3D),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            blurRadius: 8,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: Row(
-          children: [
-            _buildIconButton(Icons.directions_bus, () {
-              print("Profile tapped");
-            }),
-            const Spacer(),
-            _buildIconButton(Icons.home, () {
-              print("home");
-            }),
-            const Spacer(),
-            _buildIconButton(Icons.logout, () {
-              print("Call tapped");
-            }),
-          ],
-        ),
       ),
     );
   }
