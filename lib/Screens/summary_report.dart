c42b9cb5f500c1f62448bb189129a3b542a3b9ac
Mohammed Alnaim <moalnaim@outlook.com>
2024-09-30 05:55:01 +0300

    build: added the top and bottom bars

--- lib/Screens/summary_report.dart
+++ lib/Screens/summary_report.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kfupm_smart_bus_system/main_screen/bottom_bar.dart';
+import 'package:kfupm_smart_bus_system/main_screen/top_app_bar.dart';
 
 class SummaryPage extends StatelessWidget {
   const SummaryPage({super.key});
@@ -7,6 +9,19 @@ class SummaryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return _generateSummaryReport(context);
   }
+
+
   Widget _generateSummaryReport(BuildContext context) {
-    return Container(); 
-  }}
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            TopAppBar(),
+            BottomBar(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
