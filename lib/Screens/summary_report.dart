6b0bcba7ac12844a943612482de742978a33ec1a
Mohammed Alnaim <moalnaim@outlook.com>
2024-10-01 01:54:13 +0300

    feat: created the private _buildRequestNumberCard method that shows the styled request number

--- lib/Screens/summary_report.dart
+++ lib/Screens/summary_report.dart
@@ -10,16 +10,20 @@ class SummaryPage extends StatelessWidget {
     return _generateSummaryReport(context);
   }
 
-
   Widget _generateSummaryReport(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
             TopAppBar(),
-            const Expanded(
+            Expanded(
               child: SingleChildScrollView(
-                
+                child: Column(
+                  children: [
+                    _buildRequestNumberCard(),
+                    _generateData(),
+                  ],
+                ),
               ),
             ),
             BottomBar(),
@@ -28,5 +32,72 @@ class SummaryPage extends StatelessWidget {
       ),
     );
   }
-}
 
+  Widget _generateData() {
+    return const Column(
+      children: [
+        Text("TEST"),
+        Text("TEST1"),
+        Text("TEST2"),
+        Text("TEST3"),
+        Text("TEST4"),
+        Text("TEST5"),
+        Text("TEST6"),
+        Text("TEST7"),
+        Text("TEST8"),
+        Text("TEST9"),
+        Text("TEST10"),
+      ],
+    );
+  }
+
+  Widget _buildRequestNumberCard() {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color(0xFF179C3D),
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromARGB(255, 0, 0, 0),
+            blurRadius: 5.0,
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Request number',
+                style: TextStyle(
+                  fontSize: 21,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: const Text(
+                  '#21459',
+                  style: TextStyle(
+                    fontSize: 21,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
+}
