1ac869cd72e17f689357811560c9ffa672515125
MalikAlharbi <malik.f.alharbi@gmail.com>
2024-10-07 21:23:12 +0300

    refactor(Google Maps): main changes + TrackBus child added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:kfupm_smart_bus_system/Widgets/Welcoming.dart';
-
-import 'package:kfupm_smart_bus_system/screens/report_problem_page_two.dart';
-//import 'package:kfupm_smart_bus_system/Landscape.dart';
-import 'screens/widget_main.dart';
+import 'screens/track_bus.dart';
 import 'package:kfupm_smart_bus_system/main_screen/bottom_bar.dart';
 import 'package:kfupm_smart_bus_system/main_screen/top_app_bar.dart';
-import 'package:kfupm_smart_bus_system/screens/report_problem_page_one.dart';
-// void main() {
-//   runApp(const MaterialApp(home:
-//   Widgetmain())
-
-//   );
-// }
 
 void main() {
   runApp(MaterialApp(
@@ -23,15 +12,15 @@ void main() {
     home: Scaffold(
       resizeToAvoidBottomInset: false,
       body: SafeArea(
-        // padding: EdgeInsets.all(8),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TopAppBar(),
             const Spacer(),
-            SizedBox(
+            const SizedBox(
               height: 630,
-              child: ReportProblemPageTwo(),
+              child:
+                  TrackBus(), // This will now use the Scaffold from TrackBus.
             ),
             const Spacer(),
             BottomBar(),
@@ -41,85 +30,3 @@ void main() {
     ),
   ));
 }
-
-// Widget _buildBottomBar() {
-//   return Container(
-//     margin: const EdgeInsets.all(16.0),
-//     decoration: BoxDecoration(
-//       borderRadius: BorderRadius.circular(20),
-//       color: const Color(0xFF179C3D),
-//       boxShadow: [
-//         BoxShadow(
-//           color: Colors.black.withOpacity(0.1),
-//           blurRadius: 8,
-//           offset: const Offset(0, 2),
-//         ),
-//       ],
-//     ),
-//     child: Padding(
-//       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-//       child: Row(
-//         children: [
-//           _buildIconButton(Icons.directions_bus, () {
-//             print("Profile tapped");
-//           }),
-//           const Spacer(),
-//           _buildIconButton(Icons.home, () {
-//             print("home");
-//           }),
-//           const Spacer(),
-//           _buildIconButton(Icons.logout, () {
-//             print("Call tapped");
-//           }),
-//         ],
-//       ),
-//     ),
-//   );
-// }
-
-// Widget _buildIconButton(IconData icon, VoidCallback onTap) {
-//   return InkWell(
-//     onTap: onTap,
-//     child: Icon(
-//       icon,
-//       size: 32,
-//       color: Colors.white,
-//     ),
-//   );
-// }
-
-// Widget _buildTopBar() {
-//   return Container(
-//     margin: const EdgeInsets.all(5.0),
-//     decoration: BoxDecoration(
-//       borderRadius: BorderRadius.circular(20),
-//       color: const Color(0xFF179C3D),
-//       boxShadow: [
-//         BoxShadow(
-//           color: Colors.black.withOpacity(0.1),
-//           blurRadius: 8,
-//           offset: const Offset(0, 2),
-//         ),
-//       ],
-//     ),
-//     child: Padding(
-//       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-//       child: Row(
-//         children: [
-//           _buildIconButton(Icons.account_circle_outlined, () {
-//             print("Profile tapped");
-//           }),
-//           const Spacer(),
-//           const Image(
-//             image: AssetImage("assets/images/kfupm_logo.png"),
-//             width: 57,
-//           ),
-//           const Spacer(),
-//           _buildIconButton(Icons.call, () {
-//             print("Call tapped");
-//           }),
-//         ],
-//       ),
-//     ),
-//   );
-// }
