b48f935431217c3d0ab7742f63860c686986c53f
Ghali Alharbi <gash20190@gmail.com>
2024-10-08 23:00:18 +0300

    feat: report problem pages along with updated yaml file (camera feature)

---
+++ lib/Screens/report_problem_screen.dart
@@ -0,0 +1,194 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ReportProblemScreen extends StatefulWidget {
+  @override
+  _ReportProblemScreenState createState() => _ReportProblemScreenState();
+}
+
+class _ReportProblemScreenState extends State<ReportProblemScreen> {
+  String _selectedProblemType = 'Non-Technical Problem';
+  TextEditingController _busNumberController = TextEditingController();
+  TextEditingController _problemDescriptionController = TextEditingController();
+  bool isVisible = true;
+  bool CameraVisible = false;
+  File? image;
+
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
+
+      final imageTemporary = File(image.path);
+      setState(() {
+        this.image = imageTemporary;
+      });
+    } on PlatformException catch (m) {
+      print('Failed to pick image: $m');
+    }
+  }
+
+  void changedState(bool isVisible) {
+    setState(() {
+      isVisible = isVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Column(
+            children: [
+              Text(
+                'Report Problem',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              elevation: 8,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Problem Type',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 8),
+                    DropdownButtonFormField<String>(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                      ),
+                      value: _selectedProblemType,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedProblemType = newValue!;
+                          isVisible =
+                              newValue == "Technical Problem" ? false : true;
+                          changedState(isVisible);
+                        });
+                      },
+                      items: <String>[
+                        'Non-Technical Problem',
+                        'Technical Problem'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            SizedBox(height: 16),
+            ElevatedButton.icon(
+              icon: const Icon(
+                Icons.camera_alt,
+                color: Colors.black,
+              ),
+              label: const Text(
+                'Provide Screenshot',
+                style: TextStyle(color: Colors.white, fontSize: 22),
+              ),
+              onPressed: () {
+                // Implement screenshot functionality
+                pickImage();
+                CameraVisible = true;
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                padding: EdgeInsets.symmetric(vertical: 12),
+              ),
+            ),
+            // const SizedBox(height: 16),
+            
+            Visibility(
+              visible: CameraVisible,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20,top: 16,bottom: 0),
+                
+                child: image != null
+                    ? Image.file(
+                      
+                        image!,
+                        width: 160,
+                        height: 160,
+                        fit: BoxFit.cover,
+                        
+                      )
+                    : const SizedBox(
+                        height: 0,
+                      ),
+              ),
+            ),
+
+            const SizedBox(height: 16),
+
+            Visibility(
+              visible: isVisible,
+              child: TextField(
+                controller: _busNumberController,
+                decoration: const InputDecoration(
+                  labelText: 'Bus Number',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.directions_bus),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+
+
+            SizedBox(height: 16),
+            TextField(
+              controller: _problemDescriptionController,
+              decoration: const InputDecoration(
+                labelText: 'Explain the Problem',
+                border: OutlineInputBorder(),
+                alignLabelWithHint: true,
+              ),
+              maxLines: 5,
+            ),
+            SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                // Implement submit functionality
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                textStyle: const TextStyle(fontSize: 18),
+              ),
+              child: const Text(
+                'Submit',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
