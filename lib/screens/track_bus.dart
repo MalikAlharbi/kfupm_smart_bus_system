c235b2ddbc315462180c6e6248c1a78760564365
MalikAlharbi <malik.f.alharbi@gmail.com>
2024-10-28 20:32:50 +0300

    feat(Google Maps): display buses location in Google Maps API

--- lib/screens/track_bus.dart
+++ lib/screens/track_bus.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:kfupm_smart_bus_system/api/api_service.dart';
+import 'dart:async';
 
 class TrackBus extends StatefulWidget {
   const TrackBus({super.key});
@@ -9,18 +11,52 @@ class TrackBus extends StatefulWidget {
 }
 
 class _TrackBusState extends State<TrackBus> {
-  late GoogleMapController mapController;
+  //timer
+  Timer? timer;
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(
+        const Duration(seconds: 5), (Timer t) => _getBusesLocation());
+  }
 
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  //google maps and LatLng values
+  late GoogleMapController mapController;
   final LatLngBounds kfupmBounds = LatLngBounds(
     southwest: const LatLng(26.302883027647383, 50.134502224126315),
     northeast: const LatLng(26.314681, 50.156939),
   );
-
   final LatLng kfupmCenter =
       const LatLng(26.307048543732158, 50.145802165049304);
 
+  // buses location
+  final List<Marker> _markers = [];
+  Future<void> _getBusesLocation() async {
+    List busesLocation = await getAssetsLatestPositions();
+    BitmapDescriptor busIcon = await BitmapDescriptor.asset(
+        const ImageConfiguration(size: Size(24, 24)),
+        'assets/images/bus_icon.png');
+    setState(() {
+      _markers.clear();
+      for (var bus in busesLocation) {
+        _markers.add(Marker(
+          markerId: MarkerId(bus['assetId'].toString()),
+          position: LatLng(bus['locationLog'][0], bus['locationLog'][1]),
+          icon: busIcon,
+        ));
+      }
+    });
+  }
+
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    _getBusesLocation();
   }
 
   @override
@@ -32,12 +68,13 @@ class _TrackBusState extends State<TrackBus> {
           target: kfupmCenter,
           zoom: 15.0,
         ),
-        minMaxZoomPreference: const MinMaxZoomPreference(15.0, 18.0),
+        minMaxZoomPreference: const MinMaxZoomPreference(15.0, 21.0),
         cameraTargetBounds: CameraTargetBounds(kfupmBounds),
         //Restrict to 2D movment only
         compassEnabled: false,
         tiltGesturesEnabled: false,
         rotateGesturesEnabled: false,
+        markers: Set.from(_markers),
       ),
     );
   }
